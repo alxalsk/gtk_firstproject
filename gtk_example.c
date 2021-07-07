55f05ff8faab85d6e6bd293c43fb7cb55c39a84d
Your Name <you@example.com>
2021-07-07 12:28:05 +0200

    version pb avec structure (pointeurs multiples en arg de la fct CALLBACK)

--- gtk_example.c
+++ gtk_example.c
@@ -1,23 +1,40 @@
 #include<gtk.h>
-
-static void clicked_button(GtkWidget *widget, gpointer data)
+typedef struct{
+	GtkWidget *widget1, *widget2;
+} *maxiwidget;
+static void clicked_button3(GtkWidget *widget, gpointer data)
 {
-	g_print("OK this %s is clicked", (gchar *)data);
+	if (gtk_entry_get_text(GTK_ENTRY(data->widget1)) == "Hello")
+	gtk_button_clicked(GTK_BUTTON(data->widget2));
 }
 int main(int argc, char *argv[])
 {
-	GtkWidget *window, *label, *button;
-	gtk_init( &argc, &argv );
+	//Initialization
+	GtkWidget *window, *button, *hbox, *entry;
+	maxiwidget g = NULL;
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 00);
+	button = gtk_button_new_with_mnemonic("_Hi!");
+	entry = gtk_entry_new();
+	g->widget1 = entry;
+	g->widget2 = button;
+	
+	gtk_init( &argc, &argv );
+	
+	//<< Mise en forme >>
 	gtk_widget_set_size_request(window, 300, 300);
-//		label = gtk_label_new("Hello World");
-	button = gtk_button_new_with_label("Hi!");
-//	gtk_container_add(GTK_CONTAINER(window), label);
-	gtk_container_add(GTK_CONTAINER(window), button);
-//	gtk_label_set_text(GTK_LABEL(label), "World Hello");
+//	gtk_window_set_title(GTK_WINDOW(window), "Hello W");
+	gtk_widget_set_size_request(button, 100, 100);
+	gtk_container_add(GTK_CONTAINER(window), hbox);
+	gtk_box_pack_start(GTK_BOX(hbox), entry, 0, 0, 00);
+	gtk_box_pack_start(GTK_BOX(hbox), button, 0, 0, 0);
+//	gtk_widget_set_size_request(button, 50, 50);
+
 	gtk_widget_show_all(window);
+
+	g_signal_connect(entry, "activate", G_CALLBACK(clicked_button3), (gpointer)g);
+	g_signal_connect(button, "clicked", G_CALLBACK(clicked_button3), (gpointer) g);
 	g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit), NULL);
-	g_signal_connect(button,"clicked",G_CALLBACK(clicked_button), (gpointer)"button0");
 	gtk_main();
 	return 0;
 }
